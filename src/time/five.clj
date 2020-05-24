71665c60bbac5f3250529639df14badc2e0005f0
Pierre Larochelle <pierrebombay@gmail.com>
2020-05-24 11:04:24 -0700

    Refactoring-out offset logic

--- src/time/five.clj
+++ src/time/five.clj
@@ -5,64 +5,22 @@
 
 (def all-zones (t/available-zone-ids))
 
-(defn zone-id-to-offset
-  "Returns the offset (like \"+9:00\") for a given zone id.
-
-  The `zid` is the string representation, not the java object"
-  [zid-str]
-  (-> zid-str t/zone-id t/zoned-date-time t/zone-offset .getId))
-
-(defn offset-zid
-  "Maps offsets to given `zids`"
-  [zids]
-  (reduce (fn [acc zid]
-            (update acc
-                    (zone-id-to-offset zid)
-                    conj
-                    zid))
-          {}
-          zids))
-
-(def offset-zones (offset-zid all-zones))
-
-(def offsets (keys offset-zones))
-
 (def fiveoclock 17)
-
-(defn offset-to-zones [offset-time]
-  (.getZone (t/zoned-date-time offset-time)))
-
-(defn- with-offset [from-time offset]
-  (t/with-offset-same-instant
-    (t/offset-date-time from-time)
-    offset))
 
 (defn- hour [from-time]
   (t/as from-time :hour-of-day))
 
-(defn zoned-times-offset-with-hour
-  "ZonedDateTimes within `offsets` matching `wanted-hour` from `from-time`"
-  [from-time wanted-hour offsets]
-  (let [all-times (map #(with-offset from-time %) offsets)
-        wanted-times (filter #(= wanted-hour (hour %)) all-times)]
-    (map t/zoned-date-time wanted-times)))
-
-(defn offsets-with-hour
-  "Returns all the offsets from `from-time` that are the same as `match-hour`"
-  [from-time match-hour offsets]
-  (filter #(= match-hour (hour (with-offset from-time %))) offsets))
+(defn zones-in-hour [wanted-hour current-time zids]
+  (filter #(= wanted-hour
+              (hour (t/with-zone-same-instant current-time %)))
+          zids))
 
 (defn fiveoclock-at
   "Returns all the principal cities where its 5 o'clock and their time"
   []
-  (let [time (t/offset-date-time)
+  (let [time (t/zoned-date-time)
         wanted-hour fiveoclock
-        offset-cities offset-zones
-        all-offsets offsets
-        five-offsets (offsets-with-hour time
-                                        wanted-hour
-                                        all-offsets)
-        matching-zones (reduce concat (map offset-cities five-offsets))
+        matching-zones (zones-in-hour wanted-hour time all-zones)
         zones (remove #(-> % zones/zone-cities empty?) matching-zones)
         zone-cities (zipmap zones (map zones/zone-cities zones))
         all-cities (reduce concat (vals zone-cities))
